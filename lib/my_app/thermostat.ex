c7e52fdbe9a1c8e55b975c7ed4be734e8260f546
Leo Melo <pgp@leomeloxp.dev>
2024-02-21 08:52:33 +0000

    refactor: use pattern matching instead of cond to handle different thermostat units

--- lib/my_app/thermostat.ex
+++ lib/my_app/thermostat.ex
@@ -9,24 +9,27 @@ defmodule MyApp.Thermostat do
     }
   end
 
-  def get_temperature(%MyApp.Thermostat{} = thermostat) do
-    cond do
-      thermostat.unit == "K" -> thermostat.temperature |> Float.round(1)
-      thermostat.unit == "°F" -> ((thermostat.temperature - @kelvin_base) * 9 / 5) + 32 |> Float.round(1)
-      true -> thermostat.temperature - @kelvin_base |> Float.round(1)
-    end
+  def get_temperature(%MyApp.Thermostat{unit: "K", temperature: temperature }) do
+    temperature |> Float.round(1)
+  end
+  def get_temperature(%MyApp.Thermostat{unit: "°F", temperature: temperature }) do
+    ((temperature - @kelvin_base) * 9 / 5) + 32 |> Float.round(1)
+  end
+  def get_temperature(%MyApp.Thermostat{unit: "°C", temperature: temperature }) do
+    temperature - @kelvin_base |> Float.round(1)
   end
 
   def set_temperature(%MyApp.Thermostat{} = thermostat, new_temperature) do
     %{thermostat | temperature: new_temperature}
   end
 
+  def toggle_unit(%MyApp.Thermostat{unit: "°C"} = thermostat) do
+    %{thermostat | unit: "°F"}
+  end
+  def toggle_unit(%MyApp.Thermostat{unit: "°F"} = thermostat) do
+    %{thermostat | unit: "K"}
+  end
   def toggle_unit(%MyApp.Thermostat{} = thermostat) do
-    new_unit = cond do
-      thermostat.unit == "°C" -> "°F"
-      thermostat.unit == "°F" -> "K"
-      true -> "°C"
-    end
-    %{thermostat | unit: new_unit}
+    %{thermostat | unit: "°C"}
   end
 end
